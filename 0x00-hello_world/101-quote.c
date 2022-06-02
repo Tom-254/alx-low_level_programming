644dd73df8cfe602baaee1db15c19b8265b4e2f2
Tom-254 <211mseeratiego@gmail.com>
2022-06-02 16:59:22 -0700

    C Helloworld

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,7 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-	char *ptr = str;
 
-	fwrite(ptr, sizeof(str), 1, stderr);
+	fwrite(str, sizeof(str), 1, stderr);
 	return (1);
 }
