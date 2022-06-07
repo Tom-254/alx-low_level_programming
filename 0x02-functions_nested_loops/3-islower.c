c9038db4029feaba4e6f81847f8582ad2fcbffe0
Tom-254 <211mseeratiego@gmail.com>
2022-06-07 16:47:06 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,11 @@
 #include "main.h"
 
 /**
- * _islower - writes to stdout
+ * _islower - writes the character c to stdout
+ * @c: The character to check
  *
  * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _islower(int c)
 {
