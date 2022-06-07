4008b16dc7483d37c7da93ef136cbcb22cacf432
Tom-254 <211mseeratiego@gmail.com>
2022-06-07 16:15:04 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,14 @@
 #include "main.h"
 
 /**
- * _isalpha - writes to stdout
+ * _isalpha - writes the character c to stdout
+ * @c: The character to print
  *
  * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _isalpha(int c)
-{	
+{
 	if (isalpha(c))
 		return (1);
 	else
