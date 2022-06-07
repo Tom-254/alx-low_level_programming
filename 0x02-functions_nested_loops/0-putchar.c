ca287870739c8e69c01f27475fa1de9941f81dbc
Tom-254 <211mseeratiego@gmail.com>
2022-06-07 10:50:23 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - writes the character c to stdout
+ *
+ * Return: On success 1.
+ */
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+}
