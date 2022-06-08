7864d0c0f8e0ca451e4255983264b1702bf4f880
Tom-254 <211mseeratiego@gmail.com>
2022-06-07 17:19:36 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,6 +3,18 @@
 #include <unistd.h>
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
  * print_sign - writes the character c to stdout
  * @n: The character to print
  *
@@ -13,17 +25,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		write(1, '+', 10);
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		write(1, '0', 10);
+		_putchar('0');
 		return (0);
 	}
 	else if (n < 0)
 	{
-		write(1, '-', 10);
+		_putchar('-');
 		return (-1);
 	}
 	return (0);
