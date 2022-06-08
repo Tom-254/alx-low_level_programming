72b9ca22ab75b50718fa498969b5d3b0c332dbb4
Tom-254 <211mseeratiego@gmail.com>
2022-06-07 17:33:49 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,23 @@
 #include "main.h"
 #include <stdio.h>
-#include <unistd.h>
 
 /**
- * _printchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _printchar(char c)
-{
-		return (write(1, &c, 1));
-}
-
-/**
- * main - writes the character c to stdout
+ * main - writes to stdout
  *
  * Return: On success 1.
  */
 int main(void)
 {
-	_printchar("_putchar\n");
+	char str[] = "_putchar";
+	int count = 0;
+	int size = sizeof(str);
+
+	while (count <= size)
+	{
+		if (str[count] != '\0')
+			putchar(str[count]);
+		putchar('\n');
+		count++;
+	}
 	return (0);
 }
