365e39821db2371c1a6d40bee7a023c0e5c440e9
Tom-254 <211mseeratiego@gmail.com>
2022-06-09 17:58:33 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,27 @@ void fizz_buzz(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-			printf("FizzBuzz ");
-		else if ((i % 3) == 0)
-			printf("Fizz ");
-		else if ((i % 5) == 0)
-			printf("Buzz ");
+		if (i != 100)
+		{
+			if (i % 15 == 0)
+				printf("FizzBuzz ");
+			else if ((i % 3) == 0)
+				printf("Fizz ");
+			else if ((i % 5) == 0)
+				printf("Buzz ");
+			else
+				printf("%d ", i);
+		}
 		else
-			printf("%d ", i);
+		{
+			printf("%d", i);
+		}
 	}
 }
 
 /**
 * main - fizBuzz challenge
-* Return: always (0);
+* Return: always (0)
 */
 
 int main(void)
