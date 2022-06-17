71c81671595fae51d86d8b58e4ea17f339d76313
Tom-254 <211mseeratiego@gmail.com>
2022-06-17 04:13:10 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,22 +2,19 @@
 
 /**
  * reverse_array -   content of an array of integers.
- * @a: points to a
+ * @a: points to to the array to be reversed
  * @n: number of elements of the array
  * Return: nothing
  */
 
 void reverse_array(int *a, int n)
 {
-	int temp[1], i;
+	int i, j, temp;
 
-	for (i = 0; i < n; i++)
+	for (i = 0, j = n - 1; i < j; i++, j--)
 	{
-		temp[i] = *(a + i);
-	}
-
-	for (i = 0; i < n; i++)
-	{
-		*(a + i) = temp[n - i - 1];
+		temp = *(a + i);
+		*(a + i) = *(a + j);
+		*(a + j) = temp;
 	}
 }
