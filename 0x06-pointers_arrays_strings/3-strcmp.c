1954fa90851f953bd9fb388563f1315699ad85da
Tom-254 <211mseeratiego@gmail.com>
2022-06-17 05:53:14 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int flag = 0;
+	int flag = 0, i = 0;
 
-	while (*s1 != '\0' && *s2 != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 <  *s2)
+		if (s1[i] <  s2[i])
 		{
 			flag = -15;
 		}
-		else if (*s1 > *s2)
+		else if (s1[i] > s2[i])
 		{
 			flag = 15;
 		}
@@ -25,8 +25,7 @@ int _strcmp(char *s1, char *s2)
 		{
 			flag = 0;
 		}
-		s1++;
-		s2++;
+		i++;
 
 	}
 
