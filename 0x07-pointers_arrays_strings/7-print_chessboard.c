3c64b6ff7e6d4713ce82bcc114fe4b1eff677b8e
Tom-254 <211mseeratiego@gmail.com>
2022-06-20 18:28:15 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -15,10 +15,12 @@ void print_chessboard(char (*a)[8])
 		for (j = 0; j < 8; j++)
 		{
 			if (j == 7)
+			{
 				_putchar(a[i][j]);
 				_putchar('\n');
-
-			_putchar(a[i][j]);
+			}
+			else
+				_putchar(a[i][j]);
 		}
 	}
 }
