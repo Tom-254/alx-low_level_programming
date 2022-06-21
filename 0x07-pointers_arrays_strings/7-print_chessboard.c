6bb45cb4e6b931590c1e242c00dcb8899600a108
Tom-254 <211mseeratiego@gmail.com>
2022-06-20 17:55:46 -0700

    0x03. C - Debugging

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard -  prints the chessboard.
+ * @a: points to a
+ * Return: Nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int j, i;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		if ((i + 1) != 8)
+			putchar('\n');
+	}
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -5,3 +5,4 @@
 - 4-strpbrk.c ==>	 searches a string for any of a set of bytes.
 - 5-strstr.c ==>	 locates a substring.
 - 8-print_diagsums.c ==>	 prints the sum of the two diagonals of a square matrix of integers.
+- 7-print_chessboard.c ==>	 prints the chessboard.
