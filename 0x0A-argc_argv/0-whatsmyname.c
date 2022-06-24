b4d446772efad86ed8d26d21431f8f3bf2a784d0
Tom-254 <211mseeratiego@gmail.com>
2022-06-24 16:57:46 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main -  prints its name, followed by a new line.
+ * @argc: count of argv items
+ * @argv: array of strings of arguments
+ * Return: Nothing
+ */
+
+void main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+}
