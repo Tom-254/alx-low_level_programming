767aa2ff613cb6c560ad04344db83181b5451b90
Tom-254 <211mseeratiego@gmail.com>
2022-06-28 01:43:00 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,44 +14,28 @@ char *str_concat(char *s1, char *s2)
 	int count1 = 0, count2 = 0, total;
 
 	if (s1 == NULL || s2 == NULL)
-	{
 		return (NULL);
-	}
 
 	while (s1[count1] != '\0')
-	{
 		count1++;
-	}
 
 	while (s2[count2] != '\0')
-	{
 		count2++;
-	}
 
 	str = malloc(sizeof(*str) * (count1 + count2 + 1));
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
 
-	count1 = 0;
-	count2 = 0;
-	total = 0;
+	count1 = 0, count2 = 0, total = 0;
 
 	while (s1[count1] != '\0')
 	{
 		str[total] = s1[count1];
-		count1++;
-		total++;
+		count1++, total++;
 	}
-
 	while (s2[count2] != '\0')
-	{
-		str[total] = s2[count2];
-		count2++;
-		total++;
-	}
+		str[total] = s2[count2], count2++, total++;
 
 	str[total] = '\0';
 
