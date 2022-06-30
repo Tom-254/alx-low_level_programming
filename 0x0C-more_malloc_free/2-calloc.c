ab2852192cf9f5343d761bb4ffa15fb0bb8c2704
Tom-254 <211mseeratiego@gmail.com>
2022-06-30 07:11:24 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * string_nconcat -   allocates memory for an array, using malloc.
+ * _calloc -   allocates memory for an array, using malloc.
  * @nmemb: number of items to be
  * @size: size of each item to be
  * Return: Returns a pointer to the allocated memory
@@ -9,18 +9,21 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ar;
+	char *array;
 	unsigned int i;
 
-	if (size == 0 || nmemb == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ar = malloc(size * nmemb);
-	if (ar == NULL)
+	array = malloc(nmemb * size);
+
+	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i < (size * nmemb); i++)
-		ar[i] = 0;
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		array[i] = 0;
+	}
 
-	return (ar);
+	return (array);
 }
