9ae862062a3263154ee6dcf5d7fe6623d13403f6
Tom-254 <211mseeratiego@gmail.com>
2022-07-01 03:35:12 -0700

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include "main.h";
+
+/**
+ * _realloc -  reallocates a memory block using malloc and free
+ * @ptr: old memory allocated
+ * @old_size: old memory allocated size
+ * @new_size: new memory allocated size
+ * Return: Returns a pointer to the allocated memory
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		return (new_ptr);
+	}
+
+
+	if (new_size == 0 && !ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+
+	if (!new_ptr)
+		return (NULL);
+
+	for (i = 0; i < old_size; i++)
+		new_ptr[i] = ptr[i];
+
+	free(ptr);
+	return (new_ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -2,3 +2,4 @@
 - 1-string_nconcat.c ==>	  concatenates two strings.
 - 2-calloc.c ==>	  allocates memory for an array, using malloc.
 - 3-array_range.c ==>	   creates an array of integers.
+- 100-realloc.c ==>	reallocates a memory block using malloc and free
