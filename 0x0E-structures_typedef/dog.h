9bcade924a901bcaa999b461dcdc88d263e1ed9e
Tom-254 <211mseeratiego@gmail.com>
2022-07-04 15:05:37 -0700

    0x0D. C - Preprocessor

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef __HEADER_FILE__
+#define __HEADER_FILE__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - Dog struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+/**
+ * dog_t - Typedef for dog
+ */
+typedef struct dog dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
