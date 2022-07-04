817c48cd64525c15e7ada4f32119342e4e707391
Tom-254 <211mseeratiego@gmail.com>
2022-07-04 15:14:23 -0700

    0x0D. C - Preprocessor

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * free_dog -   function that frees dogs.
+ * @d: struct datatype pointer to dog_t
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	free(dog_t.name);
+	free(dog_t.owner);
+	free(d);
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -2,3 +2,4 @@
 - 1-init_dog.c ==>	initialize a variable of type struct dog
 - 2-print_dog.c ==>	 function that prints a struct dog
 - 4-new_dog.c ==>	 creates a new dog
+- 5-free_dog.c ==>	 function that frees dogs.
