65c0f18260490e4b901c0e646ba1aae583a10073
Tom-254 <211mseeratiego@gmail.com>
2022-07-06 14:40:13 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,8 +22,8 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (i < 5)
 	{
-		if (ops[i].ops == *s)
-			return (ops[i].f)
+		if (ops[i].op == *s)
+			return (ops[i].f);
 		i++;
 	}
 
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,6 @@
 
 int main(int argc, char **argv)
 {
-	int func_calc;
 	int (*calc)(int, int);
 
 	if  (argc < 4)
@@ -18,14 +17,14 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	func_calc = get_op_func(argv[2]);
-	if (func_calc == NULL)
+	calc = get_op_func(argv[2]);
+	if (calc == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", func_calc(argv[1], argv[3]));
+	printf("%d\n", calc(argv[1], argv[3]));
 
 	return (0);
 }
