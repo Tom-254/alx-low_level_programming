2d6ee008a4d07fa2357dac1d894995be12e87623
Tom-254 <211mseeratiego@gmail.com>
2022-07-07 15:32:20 -0700

    0x10. C - Variadic functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,33 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/*
+ * struct to_check - stores datatype and its function
+ * @check: type to print
+ * @func: pointer to a function for checking
+ *
+ * Description: stores functions easy way to print the right data
+ */
+struct to_check
+{
+	char *check;
+
+	void *(func)(va_list ap);
+}
+
+/**
+ * check_types - Typedef for unsigned int
+ */
+typedef struct to_check check_types;
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
