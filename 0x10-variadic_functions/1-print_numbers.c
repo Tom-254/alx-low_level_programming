24568e7b7c2cb5903fa18cd896961e89c1ce4495
Tom-254 <211mseeratiego@gmail.com>
2022-07-07 15:44:48 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,34 +1,83 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers -  prints numbers, followed by a new line.
- * @separator: string to separate the arguments
- * @n: number of parameters
- * Return: Nothing
+ * check_char - prints the char character
+ * @ap: va_list type
+ * Return: nothing
  */
-
-void print_numbers(const char *separator, const unsigned int n, ...)
+void check_char(va_ap ap)
 {
-	va_list ap;
-	unsigned int i;
+	printf("%c", va_arg(ap, char));
+}
+/**
+ * check_int - prints the int
+ * @ap: va_list type
+ * Return: nothing
+ */
+void check_int(va_ap ap)
+{
+	printf("%i", va_arg(ap, int));
+}
+/**
+ * check_float - prints the float
+ * @ap: va_list type
+ * Return: nothing
+ */
+void check_float(va_ap ap)
+{
+	printf("%f", va_arg(ap, float));
+}
+/**
+ * check_string - prints the string
+ * @ap: va_list type
+ * Return: nothing
+ */
+void check_string(va_ap ap)
+{
+	char *str;
 
-	va_start(ap, n);
-	for (i = 0; i < n; i++)
+	str = va_arg(ap, char *);
+	if (str == NULL)
+		str = "(nil)";
+
+	printf("%s", str);
+}
+/**
+ * print_all - prints anything
+ * @format: ap of types of arguments passed to function
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	check_types types[] = {
+		{"c", check_char},
+		{"i", check_int},
+		{"f", check_float},
+		{"s", check_string},
+		{NULL, NULL}
+	};
+
+	int pos = 0, loop_pos = 0;
+	va_list ap;
+	char *sep = "";
+
+	va_start(ap, format);
+
+	while (format && format[pos])
 	{
-		if (separator != NULL)
+		while (types[loop_pos].check)
 		{
-			if ((i + 1) != n)
-				printf("%u%s ", separator, va_arg(ap, unsigned int));
-			else
-				printf("%u\n", va_arg(ap, unsigned int));
+			if (format[pos] == *types[loop_pos].check)
+			{
+				printf("%s", sep);
+				types[loop_pos].f(ap);
+				sep = ", ";
+			}
+			loop_pos++;
 		}
-		else
-		{
-			if ((i + 1) != n)
-				printf("%u", va_arg(ap, unsigned int));
-			else
-				printf("%u\n", va_arg(ap, unsigned int));
-		}
+		loop_pos = 0;
+		pos++;
 	}
+	printf("\n");
 	va_end(ap);
 }
