b00a6e50ce1714fc878c1a5873e19335f4d0c2cc
Tom-254 <211mseeratiego@gmail.com>
2022-07-07 15:36:53 -0700

    0x10. C - Variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers -  prints numbers, followed by a new line.
+ * @separator: string to separate the arguments
+ * @n: number of parameters
+ * Return: Nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL)
+		{
+			if ((i + 1) != n)
+				printf("%u%s ", separator, va_arg(ap, unsigned int));
+			else
+				printf("%u\n", va_arg(ap, unsigned int));
+		}
+		else
+		{
+			if ((i + 1) != n)
+				printf("%u", va_arg(ap, unsigned int));
+			else
+				printf("%u\n", va_arg(ap, unsigned int));
+		}
+	}
+	va_end(ap);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,1 +1,2 @@
 - 0-sum_them_all.c ==>	  returns the sum of all its parameters.
+- 1-print_numbers.c ==>	  prints numbers, followed by a new line.
