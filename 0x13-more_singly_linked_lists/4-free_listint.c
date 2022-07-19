c40c5f6b520d9e59d5d064899b0cc2c3594fa965
Tom-254 <211mseeratiego@gmail.com>
2022-07-19 12:39:52 -0700

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint -  frees a listint_t list.
+ * @head: struct linked list head
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
