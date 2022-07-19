13da8159a7196f0d68ee1f33896198cba8e3c7f7
Tom-254 <211mseeratiego@gmail.com>
2022-07-19 12:46:59 -0700

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index -   inserts a new
+ * node at a given position.
+ * @head: struct linked list head
+ * @idx: the index of the list where the new
+ * node should be added.
+ * @n: data to be inserted
+ * Return: the address of the new node, or
+ * NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp = *head;
+	listint_t *new_node;
+	unsigned int index_at = 0;
+
+	new_node = malloc(sizeof(listint_t));
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	if (temp == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (temp != NULL)
+	{
+		if (index_at == idx - 1)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+		}
+		temp = temp->next;
+		index_at++;
+	}
+
+	return (new_node);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -7,3 +7,4 @@
 - 6-pop_listint.c ==>	deletes the head node of a listint_t linked list, and returns the head node’s data (n).
 - 7-get_nodeint.c ==>	 returns the nth node of a listint_t linked list.
 - 8-sum_listint.c ==>	 returns the sum of all the data (n) of a listint_t linked list.
+- 9-insert_nodeint.c ==>	  inserts a new node at a given position.
