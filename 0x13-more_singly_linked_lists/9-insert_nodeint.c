024e90d1db0c5a856fd883d04209ab0e7cba7046
Tom-254 <211mseeratiego@gmail.com>
2022-07-19 13:01:23 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,10 +43,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new_node->next = temp->next;
 			temp->next = new_node;
+			return (new_node);
 		}
 		temp = temp->next;
 		index_at++;
 	}
 
-	return (new_node);
+	return (NULL);
 }
