0715058dd6987937bd01504a9d891d38002e49a3
Tom-254 <211mseeratiego@gmail.com>
2022-07-21 13:11:15 -0700

    0x0F. C - Function pointers

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: binary to convert
+ * Return: the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int char_count = 0;
+	unsigned int n = 0;
+	int power = 0;
+
+	if (b == '\0')
+		return (0);
+
+	while (b[char_count + 1])
+		char_count++;
+
+	while (char_count >= 0)
+	{
+		if (b[char_count] == '0')
+		{
+			i--;
+			power++;
+		}
+		else if (b[char_count] == '1')
+		{
+			n += (1 << power);
+			char_count--;
+			power++;
+		}
+		else
+			return (0);
+	}
+	return (n);
+}
