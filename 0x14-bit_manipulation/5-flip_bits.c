ab4b29d595a0d6995c907b9f0f2b494a6bff7693
Tom-254 <211mseeratiego@gmail.com>
2022-07-21 13:26:35 -0700

    0x0F. C - Function pointers

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -20,7 +20,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 		return (0);
 	while (diff)
 	{
-		if (diff & bi_mask)
+		if (diff & bit_mask)
 			count_flipped++;
 		diff = diff >> bit_mask;
 	}
