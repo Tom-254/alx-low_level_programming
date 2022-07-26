b46ccd52d020b24b0edd7c9144d5e3c331d19a1a
Tom-254 <105339880+Tom-254@users.noreply.github.com>
2022-07-26 03:32:18 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,7 +25,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	file_t = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
-	while ((rd = read(file_f, buff, BUFSIZ)) > 0)
+	while (rd > 0)
+		rd = read(file_f, buff, BUFSIZ;
 		if (file_t == -1 || (write(file_t, buff, rd) != rd))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
