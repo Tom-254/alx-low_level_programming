6e4ee7bc1b0358da3c2ee074ad0d9c417e2e5a5b
Tom-254 <211mseeratiego@gmail.com>
2022-09-23 18:56:02 +0000

    0x1A. C - Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,8 +3,7 @@
 /**
  * hash_table_create - Creates a hash table
  * @size: The size of the array
- * Return: A pointer to the created hash table or NULL
- * wrong
+ * Return: A pointer to the hash table that was created.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -17,14 +16,26 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ht == NULL)
 		return (NULL);
 
-	ht->size = size;
-
 	ht->array = calloc(size, sizeof(hash_node_t));
 	if (ht->array == NULL)
 	{
 		free(ht);
 		return (NULL);
 	}
+	ht->size = size;
+	init_array(ht);
 
 	return (ht);
+}
+
+/**
+ * init - Initializes array slots to NULL
+ * @table: Pointer to a hash table struct
+ */
+void init_array(hash_table_t *table)
+{
+	unsigned long int i;
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
 }
