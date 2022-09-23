7b71ee36b122c5ab85cf56a6f6ad863158a24854
Tom-254 <211mseeratiego@gmail.com>
2022-09-23 19:11:30 +0000

    0x1A. C - Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = NULL;
 
-	if (!size || size == 0)
+	if (size == NULL || size == 0)
 		return (NULL);
 
 	ht = malloc(sizeof(hash_table_t));
