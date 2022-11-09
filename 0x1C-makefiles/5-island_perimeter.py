bf360ad5ab75b95189ba778941730794b6ad1ccb
Tom-254 <211mseeratiego@gmail.com>
2022-11-09 22:23:41 +0000

    0x15. C - File I/O

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """returns the perimeter of the
+        island described in grid
+        Args:
+            grid(list): list of integers
+    """
+
+    perimeter = 0
+    row_length = len(grid)
+    column_length = len(grid[0])
+
+    for r in range(row_length):
+        for c in range(column_length):
+            if grid[r][c] == 0:
+                continue
+            perimeter += 4
+            if r > 0:
+                perimeter -= grid[r - 1][c]
+            if c > 0:
+                perimeter -= grid[r][c - 1]
+            if r < row_length - 1:
+                perimeter -= grid[r + 1][c]
+            if c < column_length - 1:
+                perimeter -= grid[r][c + 1]
+
+    return perimeter
+
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
